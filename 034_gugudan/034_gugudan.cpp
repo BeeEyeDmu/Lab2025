72425809d50857077c237e283a8c3c3f88923f97
BeeEye Dmu <bikang@konyang.ac.kr>
2025-10-02 10:11:49 +0900

    Add files via upload

---
+++ 034_gugudan/034_gugudan.cpp
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	for (int j = 1; j <= 9; j++) {
+		for (int i = 2; i <= 5; i++)
+			printf("%d x %d = %d\t", i, j, i * j);
+		printf("\n");
+	}
+	printf("\n");
+
+	for (int j = 1; j <= 9; j++) {
+		for (int i = 6; i <= 9; i++)
+			printf("%d x %d = %d\t", i, j, i * j);
+		printf("\n");
+	}
+
+	// 곱셈표
+	printf("\n곱셈표\n");
+	for (int j = 1; j <= 9; j++) {
+		for (int i = 2; i <= 9; i++)
+			printf("%4d", i * j);
+		printf("\n");
+	}
+}
